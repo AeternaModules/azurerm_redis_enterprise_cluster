a32920e023978ca1f71510446069c0720aac2079
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "redis_enterprise_clusters" {
-  description = "All redis_enterprise_cluster resources"
-  value       = azurerm_redis_enterprise_cluster.redis_enterprise_clusters
-}
 output "redis_enterprise_clusters_hostname" {
-  description = "List of hostname values across all redis_enterprise_clusters"
-  value       = [for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : v.hostname]
+  description = "Map of hostname values across all redis_enterprise_clusters, keyed the same as var.redis_enterprise_clusters"
+  value       = { for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : k => v.hostname }
 }
 output "redis_enterprise_clusters_location" {
-  description = "List of location values across all redis_enterprise_clusters"
-  value       = [for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : v.location]
+  description = "Map of location values across all redis_enterprise_clusters, keyed the same as var.redis_enterprise_clusters"
+  value       = { for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : k => v.location }
 }
 output "redis_enterprise_clusters_minimum_tls_version" {
-  description = "List of minimum_tls_version values across all redis_enterprise_clusters"
-  value       = [for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : v.minimum_tls_version]
+  description = "Map of minimum_tls_version values across all redis_enterprise_clusters, keyed the same as var.redis_enterprise_clusters"
+  value       = { for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : k => v.minimum_tls_version }
 }
 output "redis_enterprise_clusters_name" {
-  description = "List of name values across all redis_enterprise_clusters"
-  value       = [for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : v.name]
+  description = "Map of name values across all redis_enterprise_clusters, keyed the same as var.redis_enterprise_clusters"
+  value       = { for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : k => v.name }
 }
 output "redis_enterprise_clusters_resource_group_name" {
-  description = "List of resource_group_name values across all redis_enterprise_clusters"
-  value       = [for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : v.resource_group_name]
+  description = "Map of resource_group_name values across all redis_enterprise_clusters, keyed the same as var.redis_enterprise_clusters"
+  value       = { for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : k => v.resource_group_name }
 }
 output "redis_enterprise_clusters_sku_name" {
-  description = "List of sku_name values across all redis_enterprise_clusters"
-  value       = [for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : v.sku_name]
+  description = "Map of sku_name values across all redis_enterprise_clusters, keyed the same as var.redis_enterprise_clusters"
+  value       = { for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : k => v.sku_name }
 }
 output "redis_enterprise_clusters_tags" {
-  description = "List of tags values across all redis_enterprise_clusters"
-  value       = [for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : v.tags]
+  description = "Map of tags values across all redis_enterprise_clusters, keyed the same as var.redis_enterprise_clusters"
+  value       = { for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : k => v.tags }
 }
 output "redis_enterprise_clusters_zones" {
-  description = "List of zones values across all redis_enterprise_clusters"
-  value       = [for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : v.zones]
+  description = "Map of zones values across all redis_enterprise_clusters, keyed the same as var.redis_enterprise_clusters"
+  value       = { for k, v in azurerm_redis_enterprise_cluster.redis_enterprise_clusters : k => v.zones }
 }
 
